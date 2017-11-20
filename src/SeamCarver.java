2645121f28f7c388a1f1b2467f8f49f70e3a1bd0
HJS <407410014@qq.com>
2017-11-20 22:42:07 +0800

    little update

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -6,28 +6,35 @@ public class SeamCarver {
     
     private Picture pic;
     
+    private double[][] energies;
+    
     public SeamCarver(Picture picture)                // create a seam carver object based on the given picture
     {
         if (picture == null)    throw new java.lang.IllegalArgumentException();
         this.pic = picture;
+        this.energies = new double[pic.height()][pic.width()];
+        for (int i = 0; i < pic.height(); i++) {
+            for (int j = 0; j < pic.width(); j++) {
+                energies[i][j] = energy(i, j);       // get array energies
+            }
+        }
     }
     public Picture picture()                          // current picture
     {
         return pic;
     }
-    public     int width()                            // width of current picture
+    public int width()                            // width of current picture
     {
         return pic.width();
     }
-    public     int height()                           // height of current picture
+    public int height()                           // height of current picture
     {
         return pic.height();
     }
-    public  double energy(int x, int y)               // energy of pixel at column x and row y
+    public double energy(int x, int y)               // energy of pixel at column x and row y
     {
-        if ((x == 0 || x == width() - 1) || (y == 0 || y == height() - 1))    
+        if ((x == 0 || x >= width() - 1) || (y == 0 || y >= height() - 1))    
             return 1000.0;
-        
         double dx = getDeltaXSquare(x, y);
         double dy = getDeltaYSquare(x, y);
         double ans = Math.sqrt(dx + dy);
@@ -52,25 +59,49 @@ public class SeamCarver {
                    + square(up.getRed() - down.getRed());
         return ans;
     }
-    public   int[] findHorizontalSeam()               // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam()               // sequence of indices for horizontal seam
     {
         return null;
     }
-    public   int[] findVerticalSeam()                 // sequence of indices for vertical seam
+    public int[] findVerticalSeam()                 // sequence of indices for vertical seam
     {
         return null;
     }
-    public    void removeHorizontalSeam(int[] seam)   // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam)   // remove horizontal seam from current picture
     {
-        if (height() <= 1)   throw new java.lang.IllegalArgumentException();
-        validateArray(seam);
+        validateArray(seam, Direction.Horizontal);
     }
-    public    void removeVerticalSeam(int[] seam)     // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam)     // remove vertical seam from current picture
     {
-        if (width() <= 1)   throw new java.lang.IllegalArgumentException();
-        validateArray(seam);
+        validateArray(seam, Direction.Vertical);
     }
-    private void validateArray(int[] arr) {
+    private void validateArray(int[] arr, Direction dir) {
         if (arr == null)    throw new java.lang.IllegalArgumentException();
+        
+        int len = 0;                    // for checking array's length
+        int range = 0;                  // for checking range of entries in array
+        if (dir == Direction.Vertical)          {
+            len = height();
+            range = width();
+        } else if (dir == Direction.Horizontal) {
+            len = width();
+            range = height();
+        }
+        if ((arr.length != len) || (range <= 1))  // if range == 1, no more need to remove any pixels
+            throw new java.lang.IllegalArgumentException();
+        checkArrayContent(arr, range);
+    }
+    private void checkArrayContent(int[] arr, int range) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (Math.abs(arr[i] - arr[i + 1]) > 1)      // only adjacent vertex is allowed  
+                throw new java.lang.IllegalArgumentException();
+            if (arr[i] < 0 || arr[i] > range)           // check whether entry is in the range        
+                throw new java.lang.IllegalArgumentException();
+        }
+        int last = arr.length - 1;                      // in for loop, last array entry is not checked
+        if (arr[last] < 0 || arr[last] > range)     throw new java.lang.IllegalArgumentException();
+    }
+    private enum Direction {
+        Vertical, Horizontal;
     }
 }
