b7164b32de0278b66964bff2545430364e967988
HJS <407410014@qq.com>
2017-11-22 20:27:03 +0800

    safe point commit -- complete with bugs

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -21,12 +21,6 @@ public class SeamCarver {
                 else    energies[i][j] = energy(i - 1, j - 1);       // get array energies
             }
         }
-        /*this.energies = new double[pic.height()][pic.width()];
-        for (int i = 0; i < pic.height(); i++) {
-            for (int j = 0; j < pic.width(); j++) {
-                energies[i][j] = energy(i, j);       // get array energies
-            }
-        }*/
     }
     public Picture picture()                          // current picture
     {
@@ -68,13 +62,81 @@ public class SeamCarver {
                 + square(up.getRed() - down.getRed());
         return ans;
     }
-    public int[] findHorizontalSeam()               // sequence of indices for horizontal seam
+    /* public int[] findHorizontalSeam()               // sequence of indices for horizontal seam
     {
         return null;
     }
     public int[] findVerticalSeam()                 // sequence of indices for vertical seam
     {
         return null;
+    }*/
+    public int[] findHorizontalSeam()               // sequence of indices for horizontal seam
+    {
+        // recursively find shortest path
+        int[] ans = null; 
+        for (int i = 0; i < width(); i++) {
+            ans = new int[width() + 1];
+            int[] sol = findPath(Direction.Horizontal, 0, i, ans);
+            if (ans[width()] > sol[width()])
+                ans = sol;
+        }
+        ans = trim(ans, width());
+        return ans;
+    }
+    public int[] findVerticalSeam()                 // sequence of indices for vertical seam
+    {
+        // recursively find shortest path
+        int[] ans = null; 
+        for (int i = 0; i < height(); i++) {
+            ans = new int[height() + 1];
+            int[] sol = findPath(Direction.Vertical, 0, i, ans);
+            if (ans[height()] > sol[height()])
+                ans = sol;
+        }
+        ans = trim(ans, height());
+        return ans;
+    }
+    private int[] trim(int[] ans, int len) {
+        int[] t = new int[len];
+        for (int i = 0; i < len; i++) {
+            t[i] = ans[i];
+        }
+        return t;
+    }
+    /**
+     * @param dir       direction that seam goes
+     * @param index     index of the ans array, same direction with seam
+     * @param path      the row or col that we manage now
+     * @param ans       the answer
+     * @return          the answer
+     */
+    private int[] findPath(Direction dir, int index, int path, int[] ans) {
+        if ((dir == Direction.Vertical) && (index == height())) {
+            for (int p = 0; p < height(); p++)  // get this path's length
+                ans[index] += energies[p + 1][ans[p] + 1];
+            return ans;  
+        } else if ((dir == Direction.Horizontal) && (index == width())) {
+            for (int p = 0; p < width(); p++)
+                ans[index] += energies[ans[p] + 1][p + 1];
+            return ans;  
+        }
+        if (index == 0)     ans[index] = path;
+        else                ans[index] = getMin(dir, index, ans[index - 1]) - 1;
+        findPath(dir, index + 1, ans[index], ans);
+        return ans;
+    }
+    private int getMin(Direction dir, int index, int path) {
+        double a = 0, b = 0, c = 0;
+        if (dir == Direction.Vertical) {
+            a = energies[index + 1][path];
+            b = energies[index + 1][path + 1];
+            c = energies[index + 1][path + 2];
+        } else if (dir == Direction.Horizontal) {
+            a = energies[path][index + 1];
+            b = energies[path + 1][index + 1];
+            c = energies[path + 2][index + 1];
+        }
+        return (a > b) ? (b > c ? (path + 2) : (path + 1)) : (c < a ? (path + 2) : path);
     }
     public void removeHorizontalSeam(int[] seam)   // remove horizontal seam from current picture
     {
