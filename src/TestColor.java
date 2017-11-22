91ee7ec5888b2006d894a44e72edb3cd372400fe
HJS <407410014@qq.com>
2017-11-22 23:14:24 +0800

    add test cases

--- src/TestColor.java
+++ src/TestColor.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.Picture;
 
 public class TestColor {
     public static void main(String[] args) {
-        Picture p = SCUtility.randomPicture(5, 5);
+        Picture p = SCUtility.randomPicture(5, 3);
         p.show();
     }
 }
