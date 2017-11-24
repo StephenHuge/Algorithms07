6a205f4e47388ff640c9030f4ceae998eb7b6f62
HJS <407410014@qq.com>
2017-11-24 16:02:24 +0800

    update test case

--- src/TestColor.java
+++ src/TestColor.java
@@ -1,8 +1,13 @@
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdIn;
 
 public class TestColor {
     public static void main(String[] args) {
-        Picture p = SCUtility.randomPicture(5, 3);
+        System.out.println("Input width: ");
+        int width = StdIn.readInt();
+        System.out.println("Input height: ");
+        int height = StdIn.readInt();
+        Picture p = SCUtility.randomPicture(width, height);
         p.show();
     }
 }
